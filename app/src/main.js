ef881c8d1c8b75fb48a0bd71eccd962fbbb4ca13
Art Dev <devtest33333@gmail.com>
2023-09-20 00:46:22 +0300

    Minor tweaks

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'    // Для обработки текста
 import config from 'config'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'), {
@@ -7,6 +8,12 @@ const bot = new Telegraf(config.get('TELEGRAM_TOKEN'), {
 
 bot.command('start', ctx => {
   ctx.reply('Добро пожаловать в бота. Отправьте тестовое сообщение с тезисами про историю.')
+})
+
+// Слушать какое-то сообщение, в текстовом формате. Т.е. на что бот будет реагировать
+// Только текст. Не будет реагировать на файл, голосовое, др
+bot.on(message('text'), ctx => {
+  ctx.reply('Test')
 })
 
 bot.launch()

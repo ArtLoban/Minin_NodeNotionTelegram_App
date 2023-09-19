85978c798b8011ddd4d7f315d55463d5233a6256
Art Dev <devtest33333@gmail.com>
2023-09-20 00:29:15 +0300

    Description added

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,12 @@
 import { Telegraf } from 'telegraf'
 import config from 'config'
 
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'), {
+  handlerTimeout: Infinity,     // Бесконечное ожидание Бота. Чтобы он работал и не сбрасывался
+})
 
+bot.command('start', ctx => {
+  ctx.reply('Добро пожаловать в бота. Отправьте тестовое сообщение с тезисами про историю.')
+})
 
+bot.launch()
